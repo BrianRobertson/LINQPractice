7ce36b850714a215d49eaf3367653d09fc94150a
BrianRobertson <briaonrobertson@gmail.com>
2017-04-24 14:35:53 -0500

    Initial project files added.

---
+++ LINQPracticeProblems/LINQPracticeProblems/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPracticeProblems
+{
+    class MainMenu
+    {
+        public void Start()
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
---
+++ LINQPracticeProblems/LINQPracticeProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MainMenu().Start();
+            Console.ReadLine();
+        }
+    }
+}
